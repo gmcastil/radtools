d9dba3506e8f3188917815089e4dab2537259813
George Castillo <gmcastil@gmail.com>
2015-05-10 21:52:36 -0600

    Finished function to calculate cross section and stats.  Seemed simpler to have the cross section function only need to be called with the fluence and number of events.  Passing in the name and the LET did not seem to make sense.  So, there needs to be some sort of higher level function that assembles all the data together and readies it for plotting or whatever.  Not sure how that should work yet.

--- radtools/crosssection.py
+++ radtools/crosssection.py
@@ -1,3 +1,5 @@
+# pylint: disable=C0103
+
 """
 Provides tools for generating SEE cross sections from accelerator testing
 
@@ -11,22 +13,45 @@ Notes:
 """
 from __future__ import division
 from collections import namedtuple
+from itertools import izip
 from math import sqrt
 
 from sandia import STAT_DATA
 
 Limits = namedtuple("Limits", ["lower", "upper"], verbose=False)
 
-def cross_section(data):
+def cross_section(events, fluence):
     """Builds cross section results from input data
 
     Args:
-      data (dict): Type, fluence, events, and LET
+      events (arraylike): Number of events observed
+      fluence (arraylike): Fluence for each observable
 
     Returns:
-      dict
+      dict - Cross section, lower, and upper statistical limits
+
+    The order of elements in the return data corresponds to the order
+    of the elements in both arguments.
+
+    Raises:
+      ValueError - If `events` and `fluence` are not the same length
 
     """
+    if len(events) != len(fluence):
+        raise ValueError("Arguments must be the same length")
+
+    lower = []
+    upper = []
+    result = []
+    for num, total_fluence in izip(events, fluence):
+        stats = _stats(num)
+        lower.append(stats.lower)
+        upper.append(stats.upper)
+        result.append(num / total_fluence)
+
+    return {"cross section" : result,
+            "lower limit" : lower,
+            "upper limit" : upper}
 
 def _stats(events):
     """Calculate 95% confidence intervals for small numbers of events
